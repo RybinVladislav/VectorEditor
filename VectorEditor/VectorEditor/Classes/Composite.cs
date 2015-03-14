beaa66cdb4e914964b64d75120a36e6a422ad8ad
RybinVladislav <rybin@me.com>
2015-03-14 16:33:09 +0300

    Added composite base

---
+++ VectorEditor/VectorEditor/Classes/Composite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VectorEditor
+{
+	public interface IComponent
+	{
+		void Operation();
+		void Add(IComponent element);
+		void Remove(IComponent element);
+	}
+
+	public interface IComposite : IComponent
+	{
+		void Operation();
+		void Add(IComponent element);
+		void Remove(IComponent element);
+	}
+	public interface ILeaf : IComponent
+	{
+		void Operation ();
+	}
+}
+
