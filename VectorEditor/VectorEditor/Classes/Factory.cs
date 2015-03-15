c6067a4fa50aaae7aa34ce6c0255a96a88ac757f
RybinVladislav <rybin@me.com>
2015-03-15 13:16:29 +0300

    fixed inheritance error

--- VectorEditor/VectorEditor/Classes/Factory.cs
+++ VectorEditor/VectorEditor/Classes/Factory.cs
@@ -8,14 +8,15 @@ using System.Drawing;
 namespace VectorEditor.Classes
 {
     public interface IFigureFactory {
-        IEllipse CreateEllipse();
-        IRectangle CreateRectangle();
-        ICurvePath CreateCurvePath();
-        IPolygon CreatePolygon();
+		IEllipse CreateEllipse(PointF center, float radiusX, float radiusY, Color fillColor, Color strokeColor, float strokeWidth);
+		IRectangle CreateRectangle(float top, float left, float width, float height, Color fillColor, Color strokeColor, float strokeWidth);
+		ICurvePath CreateCurvePath(PointF start, IList<CurveCoords> curves, Color fillColor, Color strokeColor, float strokeWidth);
+		IPolygon CreatePolygon(IList<PointF> points, Color fillColor, Color strokeColor, float strokeWidth);
     }
 
-    public class Factory : IFigureFactory
+	public class Factory : IFigureFactory
     {
+
         public IEllipse CreateEllipse(PointF center, float radiusX, float radiusY, Color fillColor, Color strokeColor, float strokeWidth)
         {
             return new Ellipse(center, radiusX, radiusY, fillColor, strokeColor, strokeWidth);
